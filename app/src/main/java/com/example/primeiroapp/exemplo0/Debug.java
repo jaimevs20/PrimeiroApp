ebb289f5c533c40746eb83ac0dba2ad9674462dd
jaime <jaimevalverde.jv@gmail.com>
2019-09-12 11:16:38 -0300

    Ciclo de Vida da aplicação.

---
+++ app/src/main/java/com/example/primeiroapp/exemplo0/Debug.java
@@ -0,0 +1,66 @@
+package com.example.primeiroapp.exemplo0;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Debug extends AppCompatActivity {
+    static final String TAG = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, getClassName() + "onCreate OK");
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, getClassName() + "onStart OK");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, getClassName() + "onStop OK");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, getClassName() + "onRestart OK");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, getClassName() + "onPause OK");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, getClassName() + "onResume OK");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, getClassName() + "onDestroy OK");
+    }
+
+    public String getClassName() {
+        String s = getClass().getSimpleName();
+
+        return "-> "+s;
+    }
+}
---
+++ app/src/main/java/com/example/primeiroapp/exemplo0/SegundaActivity.java
@@ -0,0 +1,16 @@
+package com.example.primeiroapp.exemplo0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.primeiroapp.R;
+
+public class SegundaActivity extends Debug {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
